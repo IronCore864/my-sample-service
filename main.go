7f792e95bc6e58aad9424fa645e8c909e00af8a3
Tiexin Guo <guotiexin@gmail.com>
2024-08-28 21:59:54 +0800

    add health check

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"log"
+	"math/rand"
 	"net/http"
 	"os/exec"
 	"time"
@@ -15,6 +16,7 @@ func main() {
 
 	router.GET("/", homePage)
 	router.GET("/backup-db", backupDB)
+	router.GET("/health", healthCheck)
 
 	router.Run()
 }
@@ -44,4 +46,12 @@ func sendPebbleNotification() error {
 		return errors.Join(errors.New("couldn't execute a pebble notify: "), err)
 	}
 	return nil
+}
+
+func healthCheck(c *gin.Context) {
+	if rand.Intn(10) == 0 {
+		c.String(http.StatusInternalServerError, "Health check failed")
+		return
+	}
+	c.String(http.StatusOK, "Health check passed")
 }
